7b419d84150d4ab01cdc48bd5e1d83f4d3df0af7
zxygentoo <zxygentoo@gmail.com>
2019-01-15 18:30:52 +0800

    minor.

--- pipeline.ml
+++ pipeline.ml
@@ -1,5 +1,8 @@
 let max_opt_iter = ref 1000
 
+let inline_threshold = Inline.threshold
+
+
 let rec optimize_pass n ast =
   Format.eprintf "==> iteration: %d@." n;
   if n = 0 then
@@ -14,6 +17,7 @@ let rec optimize_pass n ast =
     in if ast = ast_new
     then ast
     else optimize_pass (n - 1) ast_new
+
 
 let compile outchan buf =
   Id.counter := 0;
@@ -30,8 +34,10 @@ let compile outchan buf =
   |> Regalloc.f
   |> Emit.f outchan
 
+
 let compile_string str =
   compile stdout (Lexing.from_string str)
+
 
 let compile_file filename =
   let inchan = open_in (filename ^ ".ml") in
@@ -42,32 +48,3 @@ let compile_file filename =
     close_out outchan;
   with e ->
     (close_in inchan; close_out outchan; raise e)
-
-(* 
-let () =
-  let files = ref [] in
-  Arg.parse
-    [
-      (
-        "-inline",
-        Arg.Int(fun n -> Inline.threshold := n),
-        "maximum size of functions to inline"
-      );
-      (
-        "-iter",
-        Arg.Int(fun n -> max_opt_iter := n),
-        "maximum number of optimization iterations"
-      )
-    ]
-    (fun file -> files := !files @ [file])
-    (
-      "Mitou Min-Caml Compiler (C) Eijiro Sumii\n" ^
-      Printf.sprintf
-        "usage: %s [-inline m] [-iter n] ...filenames without \".ml\"..."
-        Sys.argv.(0)
-    );
-
-  List.iter (fun file -> ignore (compile_file file)) !files
- *)
-
-let inline_threshold = Inline.threshold
