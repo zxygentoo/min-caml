5c02f9a8850174f7209ea8e125c7b48cb277af7f
zxygentoo <zxygentoo@gmail.com>
2019-01-17 16:45:18 +0800

    minor.

--- alpha.ml
+++ alpha.ml
@@ -2,46 +2,63 @@ open Knormal
 
 
 let find x env =
-  try M.find x env with Not_found -> x
+  try
+    M.find x env
+  with Not_found ->
+    x
 
 
 let rec g env = function
-  | Unit -> Unit
+  | Unit ->
+    Unit
 
-  | Int(i) ->Int(i)
+  | Int(i) ->
+    Int(i)
 
-  | Float(d) -> Float(d)
+  | Float(d) ->
+    Float(d)
 
-  | Neg(x) -> Neg(find x env)
+  | Neg(x) ->
+    Neg(find x env)
 
-  | Add(x, y) -> Add(find x env, find y env)
+  | Add(x, y) ->
+    Add(find x env, find y env)
 
-  | Sub(x, y) -> Sub(find x env, find y env)
+  | Sub(x, y) ->
+    Sub(find x env, find y env)
 
-  | FNeg(x) -> FNeg(find x env)
+  | FNeg(x) ->
+    FNeg(find x env)
 
-  | FAdd(x, y) -> FAdd(find x env, find y env)
+  | FAdd(x, y) ->
+    FAdd(find x env, find y env)
 
-  | FSub(x, y) -> FSub(find x env, find y env)
+  | FSub(x, y) ->
+    FSub(find x env, find y env)
 
-  | FMul(x, y) -> FMul(find x env, find y env)
+  | FMul(x, y) ->
+    FMul(find x env, find y env)
 
-  | FDiv(x, y) -> FDiv(find x env, find y env)
+  | FDiv(x, y) ->
+    FDiv(find x env, find y env)
 
-  | IfEq(x, y, e1, e2) -> IfEq(find x env, find y env, g env e1, g env e2)
+  | IfEq(x, y, e1, e2) ->
+    IfEq(find x env, find y env, g env e1, g env e2)
 
-  | IfLE(x, y, e1, e2) -> IfLE(find x env, find y env, g env e1, g env e2)
+  | IfLE(x, y, e1, e2) ->
+    IfLE(find x env, find y env, g env e1, g env e2)
 
   | Let((x, t), e1, e2) ->
     let x' = Id.genid x in
     Let((x', t), g env e1, g (M.add x x' env) e2)
 
-  | Var(x) -> Var(find x env)
+  | Var(x) ->
+    Var(find x env)
 
   | LetRec({ name = (x, t); args; body }, e2) ->
     let env = M.add x (Id.genid x) env in
-    let ys = List.map fst args in
-    let env' = M.add_list2 ys (List.map Id.genid ys) env in
+    let arg_ids = List.map fst args in
+    let env' = M.add_list2 arg_ids (List.map Id.genid arg_ids) env in
     LetRec(
       {
         name = (find x env, t);
@@ -51,9 +68,11 @@ let rec g env = function
       g env e2
     )
 
-  | App(x, ys) -> App(find x env, List.map (fun y -> find y env) ys)
+  | App(x, ys) ->
+    App(find x env, List.map (fun y -> find y env) ys)
 
-  | Tuple(xs) -> Tuple(List.map (fun x -> find x env) xs)
+  | Tuple(xs) ->
+    Tuple(List.map (fun x -> find x env) xs)
 
   | LetTuple(xts, y, e) ->
     let xs = List.map fst xts in
@@ -64,13 +83,24 @@ let rec g env = function
       g env' e
     )
 
-  | Get(x, y) -> Get(find x env, find y env)
+  | Get(x, y) ->
+    Get(find x env, find y env)
 
-  | Put(x, y, z) -> Put(find x env, find y env, find z env)
+  | Put(x, y, z) ->
+    Put(find x env, find y env, find z env)
 
-  | ExtArray(x) -> ExtArray(x)
+  | ExtArray(x) ->
+    ExtArray(
+      (* no covert external identifier *)
+      x
+    )
 
-  | ExtFunApp(x, ys) -> ExtFunApp(x, List.map (fun y -> find y env) ys)
+  | ExtFunApp(x, ys) ->
+    ExtFunApp(
+      (* no covert external identifier *)
+      x,
+      List.map (fun y -> find y env) ys
+    )
 
 
 let convert =
