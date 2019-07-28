5c8fc862e9d9788f6c0c647343f9015d084c283e
Alex Zheng <zxygentoo@gmail.com>
2019-07-28 23:25:00 +0800

    minor

--- src/emit.ml
+++ src/emit.ml
@@ -443,7 +443,7 @@ let emit_memory oc =
 
 
 let emit_globals oc =
-  emit oc ";; heap pointer\n\(global $HP (mut i32) (i32.const 0))\n\
+  emit oc ";; heap pointer\n(global $HP (mut i32) (i32.const 0))\n\
            ;; closure pointer\n(global $CL (mut i32) (i32.const 0))\n\
            ;; 32-bit generic registers\n\
            (global $GA (mut i32) (i32.const 0))\n\
